50ba27ecbb9c8f6485889bcbc6c0c43740e88b60
Thanawat <taan02991@hotmail.com>
2020-11-08 00:07:32 +0700

    refractor(GamePlaying): seperate Modal component

---
+++ vue/src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="modal fade" id="modal" tabindex="-1">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Result</h5>
+                    </div>
+                    <div class="modal-body">
+                        <p>Winner: Banker</p>
+                        <p>Your bet: Banker</p>
+                        <p>Amount: 100</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                    </div>
+                </div>
+    </div>
+</template>
+
+<style scoped>
+.modal-content {
+  color: black;
+}
+</style>
