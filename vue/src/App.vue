69ce45a6fe4ff853e2d141433b1edaa53a453ae1
Thanawat <taan02991@hotmail.com>
2020-11-07 17:52:09 +0700

    refractor: Change google font

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,11 +5,11 @@
 </template>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Itim&display=swap');
 
 body {
   margin: 0;
-  font-family: "Inter", "Helvetica", sans-serif;
+  font-family: 'Itim', cursive;
 }
 </style>
 
