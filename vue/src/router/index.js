fb281776c3dda1c4d50aa43dfe350bf06f577c5c
Thanawat <taan02991@hotmail.com>
2020-11-07 17:52:55 +0700

    feat(GamePlaying): Add Card component, UserList component, GamePlaying Page

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Index from "../views/Index.vue";
+import GamePlaying from "@/views/GamePlaying.vue";
 
 Vue.use(VueRouter);
 
@@ -8,6 +9,10 @@ const routes = [
   {
     path: "/",
     component: Index,
+  },
+  {
+    path: "/game-playing/:id",
+    component: GamePlaying,
   },
 ];
 
