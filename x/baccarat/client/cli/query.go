c7fd6358256d7e214f524aa46ef6dea3982df5fc
Thanawat <taan02991@hotmail.com>
2020-11-18 17:44:29 +0700

    feat(register): Add user type

--- x/baccarat/client/cli/query.go
+++ x/baccarat/client/cli/query.go
@@ -30,6 +30,7 @@ func GetQueryCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 	baccaratQueryCmd.AddCommand(
 		flags.GetCommands(
       // this line is used by starport scaffolding
+			GetCmdListUser(queryRoute, cdc),
 		)...,
 	)
 
--- x/baccarat/client/rest/rest.go
+++ x/baccarat/client/rest/rest.go
@@ -9,4 +9,6 @@ import (
 // RegisterRoutes registers baccarat-related REST handlers to a router
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
   // this line is used by starport scaffolding
+	r.HandleFunc("/baccarat/user", listUserHandler(cliCtx, "baccarat")).Methods("GET")
+	r.HandleFunc("/baccarat/user", createUserHandler(cliCtx)).Methods("POST")
 }
