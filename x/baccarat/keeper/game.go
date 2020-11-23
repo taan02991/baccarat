2dd03dfc6e96abe93a831e70663f8a272a411687
Thanawat <taan02991@hotmail.com>
2020-11-23 17:16:46 +0700

    feat(game-logic): Add start game API

--- x/baccarat/keeper/game.go
+++ x/baccarat/keeper/game.go
@@ -4,6 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/blockchain/baccarat/x/baccarat/types"
   "github.com/cosmos/cosmos-sdk/codec"
+  "fmt"
 )
 
 func (k Keeper) CreateGame(ctx sdk.Context, game types.Game) {
@@ -11,6 +12,22 @@ func (k Keeper) CreateGame(ctx sdk.Context, game types.Game) {
 	key := []byte(types.GamePrefix + game.ID)
 	value := k.cdc.MustMarshalBinaryLengthPrefixed(game)
 	store.Set(key, value)
+}
+
+func (k Keeper) StartGame(ctx sdk.Context, id string) {
+  var game types.Game
+	store := ctx.KVStore(k.storeKey)
+  key := []byte(types.GamePrefix + id)
+  err := k.cdc.UnmarshalBinaryLengthPrefixed(store.Get(key), &game)
+  if err != nil {
+    fmt.Printf("Failed to start game:\n%s\n", err.Error())
+    return
+  }
+  game.State = types.Playing
+  //Todo: generate card deck & draw first 4 cards
+  game.ResultHash = []string {"1A,2B;4S,5K"}
+	value := k.cdc.MustMarshalBinaryLengthPrefixed(game)
+  store.Set(key, value)
 }
 
 func listGame(ctx sdk.Context, k Keeper) ([]byte, error) {
