152b074f426a53df19cc5509c9529d43863a19ee
Thanawat <taan02991@hotmail.com>
2020-11-24 15:47:41 +0700

    feat(UI): Add test create user, game and join game,  fetch game state

--- vue/src/views/Index.vue
+++ vue/src/views/Index.vue
@@ -5,5 +5,37 @@
       <wallet />
       <type-list />
     </app-layout>
+    <p>Test Register</p>
+    <input class="text-black" v-model="name">
+    <button @click='register' class="bg-green-500 m-1">Register</button>
+    <p>Test Create Game</p>
+    <button @click='createGame' class="bg-green-500 m-1">CreateGame</button>
+    <p>Test Join Room</p>
+    <button v-for="e in $store.state.data.game" v-bind:key="e.id" @click="$router.push(`/game/${e.id}`)" class="bg-green-500 m-1">
+      {{e.id}}
+    </button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ""
+    }
+  },
+  methods: {
+    async register(){
+        let mnemonic = await this.$store.dispatch("accountRegister", this.name)
+        console.log(this.name)
+        alert(mnemonic)
+    },
+    async createGame(){
+        let res = await this.$store.dispatch("entitySubmit", {type: "game", body: {}})
+        console.log(res)
+        await this.$store.dispatch("entityFetch", {type: "game"})
+
+    }
+  }
+}
+</script>
