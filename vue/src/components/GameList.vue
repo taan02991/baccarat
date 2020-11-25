56feb68959787567f0202a8390aae23fb0ff9d7a
Natchapol Srisang <utopiabeam@gmail.com>
2020-11-25 23:15:59 +0700

    feat(UI): add create game button

--- vue/src/components/GameList.vue
+++ vue/src/components/GameList.vue
@@ -1,41 +1,34 @@
 <template>
   <div>
     <div class="h1 pt-6">Game list</div>
-      <table v-if="game.length > 0" class="table-auto w-full">
-        <thead>
-          <tr>
-            <th># round</th>
-            <th># participants</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="g in game" :key="g.id">
-            <td>{{ (g.result || []).length }}</td>
-            <td>{{ g.participant.length }}</td>
-            <td>
-              <button @click="$router.push(`/game/${g.id}`)">
-                Join Game
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <button class="bg-green-500" @click="createGame">
+      Create new game
+    </button>
+    <table v-if="game.length > 0" class="table-auto w-full">
+      <thead>
+        <tr>
+          <th># round</th>
+          <th># participants</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="g in game" :key="g.id">
+          <td>{{ (g.result || []).length }}</td>
+          <td>{{ g.participant.length }}</td>
+          <td>
+            <button @click="$router.push(`/game/${g.id}`)">
+              Join game
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
     <div class="card__empty" v-else>
       There are no game yet.
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  computed: {
-    game() {
-      return (this.$store.state.data.game || []).filter(g => g.state !== 'End')
-    }
-  }
-};
-</script>
 
 <style scoped>
 .h1 {
@@ -57,4 +50,41 @@ export default {
   text-align: center;
   min-height: 8rem;
 }
+button {
+  background: rgba(255, 255, 255, 0.03);
+  padding: 0.75rem;
+  white-space: nowrap;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 700;
+  font-size: 0.85rem;
+  color: rgba(255, 255, 255, 0.75);
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+  border-radius: 0.25rem;
+  transition: all 0.1s;
+  user-select: none;
+}
 </style>
+
+<script>
+export default {
+  computed: {
+    game() {
+      return (this.$store.state.data.game || []).filter(g => g.state !== "End");
+    }
+  },
+  methods: {
+    async createGame() {
+      let res = await this.$store.dispatch("entitySubmit", {
+        type: "game",
+        body: {}
+      });
+      console.log(res.value.id);
+      this.$router.push(`game/${res.value.id}`);
+    }
+  },
+};
+</script>
+
