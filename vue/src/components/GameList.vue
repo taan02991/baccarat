aa7367322abbfd9b064572a9532ad5a2ee158325
Natchapol Srisang <utopiabeam@gmail.com>
2020-11-25 23:28:42 +0700

    feat(ui): style game list

--- vue/src/components/GameList.vue
+++ vue/src/components/GameList.vue
@@ -4,27 +4,27 @@
     <button class="bg-green-500" @click="createGame">
       Create new game
     </button>
-    <table v-if="game.length > 0" class="table-auto w-full">
+    <table v-if="game.length > 0" class="table-fixed w-full mt-6">
       <thead>
-        <tr>
-          <th># round</th>
-          <th># participants</th>
-          <th></th>
+        <tr class="border-2 border-gray-500">
+          <th class="w-1/3 p-2"># round</th>
+          <th class="w-1/3 p-2"># participants</th>
+          <th class="w-1/3 p-2"></th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="g in game" :key="g.id">
-          <td>{{ (g.result || []).length }}</td>
-          <td>{{ g.participant.length }}</td>
-          <td>
-            <button @click="$router.push(`/game/${g.id}`)">
+        <tr v-for="g in game" :key="g.id" class="border-2 border-gray-500">
+          <td class="text-center">{{ (g.result || []).length }}</td>
+          <td class="text-center">{{ g.participant.length }}</td>
+          <td class="flex justify-center">
+            <button class="bg-green-500" @click="$router.push(`/game/${g.id}`)">
               Join game
             </button>
           </td>
         </tr>
       </tbody>
     </table>
-    <div class="card__empty" v-else>
+    <div class="card__empty pt-6" v-else>
       There are no game yet.
     </div>
   </div>
@@ -38,7 +38,6 @@
   margin-bottom: 1rem;
 }
 .card__empty {
-  margin-bottom: 1rem;
   border: 1px dashed rgba(255, 255, 255, 0.1);
   padding: 1rem;
   display: flex;
@@ -51,7 +50,6 @@
   min-height: 8rem;
 }
 button {
-  background: rgba(255, 255, 255, 0.03);
   padding: 0.75rem;
   white-space: nowrap;
   display: flex;
@@ -59,7 +57,7 @@ button {
   justify-content: center;
   font-weight: 700;
   font-size: 0.85rem;
-  color: rgba(255, 255, 255, 0.75);
+  color: rgb(255, 255, 255);
   text-transform: uppercase;
   letter-spacing: 0.05em;
   border-radius: 0.25rem;
